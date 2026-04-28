ceca9b7114095f514d3b2d12eb6df98d2acb255a
Aditya <aditya.takale.822@gmail.com>
2026-04-28 14:34:05 +0530

    to check conflict

--- tests/example8.spec.js
+++ tests/example8.spec.js
@@ -15,7 +15,7 @@ test('get started link', async ({ page }) => {
 });
 
 test('TC3', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/conflict2');
 });
 
 test('TC4', async ({ page }) => {
